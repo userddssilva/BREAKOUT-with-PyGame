1b4545ad515d4c22c50f9c0f16a1bcc0d86e5af8
tamaturgo <ddst.snf20@uea.edu.br>
2021-05-19 21:17:11 -0400

    Player Move, Ball Move, Colliders (ball and player)

--- BreakOut_PyGame.py
+++ BreakOut_PyGame.py
@@ -1,8 +1,66 @@
 import pygame
-from pygame.time import Clock
 
+# Screen set
 pygame.init()
-screen_size = (800, 600)
+screen_size = (1000, 720)
 screen = pygame.display.set_mode(screen_size)
- 
-    
+
+game_clock = pygame.time.Clock()
+game_cycle = True
+
+# Player
+player = pygame.image.load("assets/player.png")
+player_move_left = False
+player_move_right = False
+
+# Ball
+ball = pygame.image.load("assets/ball.png")
+ball_dx = 5
+ball_dy = 7
+ball_x = 640
+ball_y = 360
+
+pygame.mouse.set_visible(False)
+
+while game_cycle:
+    # End Game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_cycle = False
+
+    # Player Movement
+    player_XY = pygame.mouse.get_pos()
+    player_x = player_XY[0]
+    if player_x > 850:
+        player_x = 850
+
+    # Ball Movement
+    ball_x += ball_dx
+    ball_y += ball_dy
+
+    # Colliders
+    if ball_x < 0:
+        ball_dx *= -1
+    if ball_x > 1000:
+        ball_dx *= -1
+    if ball_y < 60:
+        ball_dy *= -1
+    if ball_y > 720:
+        # YOU LOSE
+        ball_dy *= -1
+
+    # Collider with Player
+    if ball_y > 680 and not (ball_y > 700):
+        if player_x < ball_x + 150:
+            if player_x + 150 > ball_x:
+                ball_y = 680
+                ball_dy *= -1
+
+    # Screen update
+    screen.fill((0, 0, 0))
+    screen.blit(player, (player_x, 680))
+    screen.blit(ball, (ball_x, ball_y))
+    pygame.display.flip()
+    game_clock.tick(60)
+
+pygame.quit()
